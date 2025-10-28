d26bb151db8fd3bcfe8f12ff2968addb00faee72
Ingemar Berghult <ingemar.kjellman@gmail.com>
2025-10-28 15:16:27 +0100

    schedule component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
+import schedule from "./schedule";
 
 function App() {
-	return (
-		<>
-			<h1>Slotify</h1>
-		</>
-	);
+  return (
+    <>
+      <h1>Slotify</h1>
+      <schedule />
+    </>
+  );
 }
 
 export default App;
---
+++ frontend/src/schedule.jsx
@@ -0,0 +1,5 @@
+const schedule = () => {
+  return <div>Schedule</div>;
+};
+
+export default schedule;
