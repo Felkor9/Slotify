903f6f8f26d9684eff05836a83ec0010128c23ff
felkor0 <felixkorshavn@gmail.com>
2025-11-03 10:38:07 +0100

    encrypted passwords and working create user

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,14 @@ const express = require("express"),
 	path = require("path");
 const dotenv = require("dotenv");
 const { Client } = require("pg");
+const { request } = require("http");
+const bcrypt = require("bcrypt");
 
 dotenv.config();
 
 const app = express();
+
+app.use(express.json());
 
 const client = new Client({
 	connectionString: process.env.PGURI,
@@ -15,6 +19,32 @@ client.connect();
 app.get("/api/user", async (_request, response) => {
 	const { rows } = await client.query('SELECT * FROM "user";');
 	response.send(rows);
+});
+
+app.post("/api/user", async (_request, response) => {
+	try {
+		const { username, email, password } = _request.body;
+		console.log("body", _request.body);
+		if (!username || !email || !password) {
+			return response.status(400).json({ error: "Du måste fylla i uppgifterna korrekt" });
+		}
+
+		const hashedPassword = await bcrypt.hash(password, 10);
+
+		const result = await client.query(
+			'INSERT INTO "user" (username, email, password)VALUES($1, $2, $3) RETURNING *',
+			[username, email, hashedPassword]
+		);
+		response.status(201).json({
+			id: result.rows[0].id,
+			username: result.rows[0].username,
+			email: result.rows[0].email,
+			isadmin: result.rows[0].isadmin,
+		});
+	} catch (err) {
+		console.error("Fel vid skapande av avdelning:", err);
+		response.status(500).json({ error: err.message });
+	}
 });
 
 app.use(express.static(path.join(path.resolve(), "dist")));
--- frontend/src/CreateUserComponent.jsx
+++ frontend/src/CreateUserComponent.jsx
@@ -1,48 +1,93 @@
 import "./CreateUserComponent.css";
 import { Link } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function CreateUserComponent() {
+	const [username, setUserName] = useState("");
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [error, setError] = useState(null);
+
+	useEffect(() => {
+		fetch("/api/user")
+			.then((response) => response.json())
+			.then((data) => console.log(data));
+	}, []);
+
+	const addUser = async () => {
+		if (!username || !email || !password) {
+			setError("Vänligen fyll i alla fält.");
+			return;
+		}
+		try {
+			const res = await fetch("/api/user", {
+				method: "POST",
+				headers: { "Content-Type": "application/json" },
+				body: JSON.stringify({ username, email, password }),
+			});
+			if (!res.ok) throw new Error("Kunde inte skapa användare");
+
+			setUserName("");
+			setEmail("");
+			setPassword("");
+			setError(null);
+		} catch (err) {
+			setError(err.message);
+		}
+	};
+
 	return (
-		useEffect(() => {
-			fetch("/api/user")
-				.then((response) => response.json())
-				.then((data) => console.log(data));
-		}, []),
-		(
-			<>
-				<div className="login-container">
-					<div className="form-div">
-						<h2 className="heading-slotify">Sign up to Slotify</h2>
-						<form className="login-form" onSubmit={(e) => e.preventDefault()}>
-							<div className="input-group">
-								<label htmlFor="username">Name:</label>
-								<input className="input-text" type="text" id="username" name="username" required />
-								<label htmlFor="email">Email:</label>
-								<input className="input-text" type="text" id="email" name="email" required />
-								<label htmlFor="password">Password:</label>
-								<input
-									className="input-text"
-									type="password"
-									id="password"
-									name="password"
-									required
-								/>
-								<div className="terms-group">
-									<label htmlFor="agree">Agree terms of use and handling of GDPR</label>
-									<input type="checkbox" name="agree" id="agree" required className="check-btn" />
-								</div>
+		<>
+			<div className="login-container">
+				<div className="form-div">
+					<h2 className="heading-slotify">Sign up to Slotify</h2>
+					<form className="login-form" onSubmit={(e) => e.preventDefault()}>
+						<div className="input-group">
+							<label htmlFor="username">Name:</label>
+							<input
+								className="input-text"
+								type="text"
+								id="username"
+								name="username"
+								value={username}
+								onChange={(e) => setUserName(e.target.value)}
+								required
+							/>
+							<label htmlFor="email">Email:</label>
+							<input
+								className="input-text"
+								value={email}
+								onChange={(e) => setEmail(e.target.value)}
+								type="text"
+								id="email"
+								name="email"
+								required
+							/>
+							<label htmlFor="password">Password:</label>
+							<input
+								className="input-text"
+								type="password"
+								id="password"
+								name="password"
+								value={password}
+								onChange={(e) => setPassword(e.target.value)}
+								required
+							/>
+							<div className="terms-group">
+								<label htmlFor="agree">Agree terms of use and handling of GDPR</label>
+								<input type="checkbox" name="agree" id="agree" required className="check-btn" />
 							</div>
-							<div className="btn-group">
-								<button className="login-btn" type="submit">
-									<Link to={"/schedule"}>Sign up</Link>
-								</button>
-							</div>
-						</form>
-					</div>
+						</div>
+						<div className="btn-group">
+							{error && <p style={{ color: "red" }}>Fel: {error}</p>}
+							<button className="login-btn" onClick={addUser} type="submit">
+								Create Account
+							</button>
+						</div>
+					</form>
 				</div>
-			</>
-		)
+			</div>
+		</>
 	);
 }
 export default CreateUserComponent;
