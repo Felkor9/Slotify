65b349337fbb8e1c027254a1bfef013a51226baa
felkor0 <felixkorshavn@gmail.com>
2025-11-03 09:05:11 +0100

    add postgres and get code in index.js, now works to build frontend from backend with npm run build-frontend

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,24 @@
-const express = require("express");
-const app = express();
-const port = 3000;
+const express = require("express"),
+	path = require("path");
+const dotenv = require("dotenv");
+const { Client } = require("pg");
 
-app.listen(port, () => {
-	console.log(`listening to ${port} `);
+dotenv.config();
+
+const app = express();
+
+const client = new Client({
+	connectionString: process.env.PGURI,
+});
+client.connect();
+
+app.get("/api/user", async (_request, response) => {
+	const { rows } = await client.query('SELECT * FROM "user";');
+	response.send(rows);
+});
+
+app.use(express.static(path.join(path.resolve(), "dist")));
+
+app.listen(3000, () => {
+	console.log("Redo på http://localhost:3000/");
 });
--- frontend/src/CreateUserComponent.jsx
+++ frontend/src/CreateUserComponent.jsx
@@ -1,40 +1,48 @@
 import "./CreateUserComponent.css";
 import { Link } from "react-router-dom";
+import { useEffect } from "react";
 
 function CreateUserComponent() {
 	return (
-		<>
-			<div className="login-container">
-				<div className="form-div">
-					<h2 className="heading-slotify">Sign up to Slotify</h2>
-					<form className="login-form" onSubmit={(e) => e.preventDefault()}>
-						<div className="input-group">
-							<label htmlFor="username">Name:</label>
-							<input className="input-text" type="text" id="username" name="username" required />
-							<label htmlFor="email">Email:</label>
-							<input className="input-text" type="text" id="email" name="email" required />
-							<label htmlFor="password">Password:</label>
-							<input
-								className="input-text"
-								type="password"
-								id="password"
-								name="password"
-								required
-							/>
-							<div className="terms-group">
-								<label htmlFor="agree">Agree terms of use and handling of GDPR</label>
-								<input type="checkbox" name="agree" id="agree" required className="check-btn" />
+		useEffect(() => {
+			fetch("/api/user")
+				.then((response) => response.json())
+				.then((data) => console.log(data));
+		}, []),
+		(
+			<>
+				<div className="login-container">
+					<div className="form-div">
+						<h2 className="heading-slotify">Sign up to Slotify</h2>
+						<form className="login-form" onSubmit={(e) => e.preventDefault()}>
+							<div className="input-group">
+								<label htmlFor="username">Name:</label>
+								<input className="input-text" type="text" id="username" name="username" required />
+								<label htmlFor="email">Email:</label>
+								<input className="input-text" type="text" id="email" name="email" required />
+								<label htmlFor="password">Password:</label>
+								<input
+									className="input-text"
+									type="password"
+									id="password"
+									name="password"
+									required
+								/>
+								<div className="terms-group">
+									<label htmlFor="agree">Agree terms of use and handling of GDPR</label>
+									<input type="checkbox" name="agree" id="agree" required className="check-btn" />
+								</div>
 							</div>
-						</div>
-						<div className="btn-group">
-							<button className="login-btn" type="submit">
-								<Link to={"/schedule"}>Sign up</Link>
-							</button>
-						</div>
-					</form>
+							<div className="btn-group">
+								<button className="login-btn" type="submit">
+									<Link to={"/schedule"}>Sign up</Link>
+								</button>
+							</div>
+						</form>
+					</div>
 				</div>
-			</div>
-		</>
+			</>
+		)
 	);
 }
 export default CreateUserComponent;
