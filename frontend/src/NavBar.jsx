8d42e686ad9af0292efe48bdcd17bb4f266e10a8
Ingemar Berghult <ingemar.kjellman@gmail.com>
2025-11-04 14:15:45 +0100

    ?

--- frontend/src/NavBar.jsx
+++ frontend/src/NavBar.jsx
@@ -4,50 +4,50 @@ import { Link } from "react-router-dom";
 import GlobalContext from "./GlobalContext";
 
 function NavBar() {
-	const [isHamMenuVisible, setIsHamMenuVisible] = useState(false);
-	const { loggedInUserId, users } = useContext(GlobalContext);
-	const [name, setName] = useState(null);
+  const [isHamMenuVisible, setIsHamMenuVisible] = useState(false);
+  const { loggedInUserId, users } = useContext(GlobalContext);
+  const [name, setName] = useState(null);
 
-	useEffect(() => {
-		if (users) {
-			setName(users.find((u) => u.id == loggedInUserId));
-		}
-	}, [users, loggedInUserId]);
+  useEffect(() => {
+    if (users) {
+      setName(users.find((u) => u.id == loggedInUserId));
+    }
+  }, [users, loggedInUserId]);
 
-	return (
-		<nav>
-			<div className="navbar-container">
-				<Link to={"/"}>
-					<h2 className="nav-header">Slotify</h2>
-				</Link>
-				<p>
-					<strong>Inloggad som:</strong> {name?.username}
-				</p>
+  return (
+    <nav>
+      <div className="navbar-container">
+        <Link to={"/"}>
+          <h2 className="nav-header">Slotify</h2>
+        </Link>
+        <p>
+          <strong>Inloggad som:</strong> {name?.username}
+        </p>
 
-				<img
-					className="hamburger-icon"
-					onClick={() => setIsHamMenuVisible(!isHamMenuVisible)}
-					width={35}
-					src="/icons8-hamburger.svg"
-					alt="hamburger menu"
-				/>
-			</div>
+        <img
+          className="hamburger-icon"
+          onClick={() => setIsHamMenuVisible(!isHamMenuVisible)}
+          width={35}
+          src="/icons8-hamburger.svg"
+          alt="hamburger menu"
+        />
+      </div>
 
-			<div className={`ham-menu-overlay ${isHamMenuVisible ? "show" : ""}`}>
-				<ul className="ham-menu-list">
-					<li>
-						<Link to="/create-user">Create account</Link>
-					</li>
-					<li>
-						<Link to="/schedule">Schedule</Link>
-					</li>
-					<li>Settings</li>
-					<li>Edit account</li>
-					<li>My bookings</li>
-				</ul>
-			</div>
-		</nav>
-	);
+      <div className={`ham-menu-overlay ${isHamMenuVisible ? "show" : ""}`}>
+        <ul className="ham-menu-list">
+          <li>
+            <Link to="/create-user">Create account</Link>
+          </li>
+          <li>
+            <Link to="/schedule">Schedule</Link>
+          </li>
+          <li>Settings</li>
+          <li>Edit account</li>
+          <li>My bookings</li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
 
 export default NavBar;
