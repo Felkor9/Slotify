ced3db4903ec5590a4066e35541e8c935f268bd3
Ingemar Berghult <ingemar.kjellman@gmail.com>
2025-11-07 10:25:37 +0100

    post-merge conflict

--- frontend/src/schedule.jsx
+++ frontend/src/schedule.jsx
@@ -10,36 +10,36 @@ const Schedule = () => {
   const [bookings, setBookings] = useState([]);
   const [alreadyBooked, setAlreadyBooked] = useState(false);
 
-	useEffect(() => {
-		fetch("/api/days")
-			.then((response) => response.json())
-			.then((data) => {
-				setDays(data);
-			})
-			.catch((error) => console.error("Error fetching days:", error));
-		fetch("/api/slots")
-			.then((response) => response.json())
-			.then((data) => {
-				setTimeSlots(data);
-			});
-		fetch("/api/seats")
-			.then((response) => response.json())
-			.then((data) => {
-				setSeats(data);
-			});
-	}, []);
+  useEffect(() => {
+    fetch("/api/days")
+      .then((response) => response.json())
+      .then((data) => {
+        setDays(data);
+      })
+      .catch((error) => console.error("Error fetching days:", error));
+    fetch("/api/slots")
+      .then((response) => response.json())
+      .then((data) => {
+        setTimeSlots(data);
+      });
+    fetch("/api/seats")
+      .then((response) => response.json())
+      .then((data) => {
+        setSeats(data);
+      });
+  }, []);
 
-	useEffect(() => {
-		fetch("/api/bookings")
-			.then((response) => response.json())
-			.then((data) => {
-				setBookings(data);
-			});
-	}, []);
+  useEffect(() => {
+    fetch("/api/bookings")
+      .then((response) => response.json())
+      .then((data) => {
+        setBookings(data);
+      });
+  }, []);
 
-	const { loggedInUserId, users } = useContext(GlobalContext);
-	console.log("users:,", users);
-	console.log("bookings:", bookings);
+  const { loggedInUserId, users } = useContext(GlobalContext);
+  console.log("users:,", users);
+  console.log("bookings:", bookings);
 
   const handleBooking = async (
     bookedUserId,
@@ -93,84 +93,94 @@ const Schedule = () => {
     }
   };
 
-	return (
-		<>
-			<div className="container">
-				<table
-					className="table-content"
-					style={{
-						borderCollapse: "collapse",
-						width: "100%",
-						textAlign: "center",
-					}}>
-					<thead className="thead-container">
-						<tr>
-							<th style={{ border: "1px solid", width: "10%" }}>Tid</th>
-							{days &&
-								days.map((day) => (
-									<th key={day.id} style={{ border: "1px solid", width: "18%" }}>
-										{day.name}
-									</th>
-								))}
-						</tr>
-					</thead>
-					<tbody>
-						{timeSlots &&
-							timeSlots.map((time) => (
-								<tr key={time.id}>
-									<td style={{ border: "1px solid" }}>
-										{time.starttime} - {time.endtime}
-									</td>
-									{days.map((day) => {
-										// const booking = bookings.find(
-										//   (slot) => slot.day === day && slot.time === time
-										// );
-										return (
-											<td key={day.id + time.id} style={{ border: "1px solid", cursor: "pointer" }}>
-												{seats.map((seat) => {
-													// const bookedUser = booking?.booked[idx]
-													//   ? users.find((u) => u.id === booking.booked[idx])
-													//       ?.username
-													//   : null;
-													const bookedUserId = bookings.find(
-														(b) =>
-															b.day_id === day.id &&
-															b.timeslots_id === time.id &&
-															b.seats_id === seat.id
-													)?.user_id;
+  return (
+    <>
+      <div className="container">
+        <table
+          className="table-content"
+          style={{
+            borderCollapse: "collapse",
+            width: "100%",
+            textAlign: "center",
+          }}
+        >
+          <thead className="thead-container">
+            <tr>
+              <th style={{ border: "1px solid", width: "10%" }}>Tid</th>
+              {days &&
+                days.map((day) => (
+                  <th
+                    key={day.id}
+                    style={{ border: "1px solid", width: "18%" }}
+                  >
+                    {day.name}
+                  </th>
+                ))}
+            </tr>
+          </thead>
+          <tbody>
+            {timeSlots &&
+              timeSlots.map((time) => (
+                <tr key={time.id}>
+                  <td style={{ border: "1px solid" }}>
+                    {time.starttime} - {time.endtime}
+                  </td>
+                  {days.map((day) => {
+                    // const booking = bookings.find(
+                    //   (slot) => slot.day === day && slot.time === time
+                    // );
+                    return (
+                      <td
+                        key={day.id + time.id}
+                        style={{ border: "1px solid", cursor: "pointer" }}
+                      >
+                        {seats.map((seat) => {
+                          // const bookedUser = booking?.booked[idx]
+                          //   ? users.find((u) => u.id === booking.booked[idx])
+                          //       ?.username
+                          //   : null;
+                          const bookedUserId = bookings.find(
+                            (b) =>
+                              b.day_id === day.id &&
+                              b.timeslots_id === time.id &&
+                              b.seats_id === seat.id
+                          )?.user_id;
 
-													const bookedUser = users.find((u) => u.id === bookedUserId)?.username;
+                          const bookedUser = users.find(
+                            (u) => u.id === bookedUserId
+                          )?.username;
 
-                        return (
-                          <div
-                            key={seat.id}
-                            style={{
-                              border: "1px solid rgb(191, 191, 191)",
-                              backgroundColor: bookedUser
-                                ? "#f5b2aaff"
-                                : "#afffa5ff",
-                              height: "24px",
-                            }}
-                            onClick={() =>
-                              handleBooking(
-                                bookedUserId,
-                                day.id,
-                                time.id,
-                                seat.id
-                              )
-                            }
-                          >
-                            {bookedUser || ""}
-                          </div>
-                        );
-                      })}
-                    </td>
-                  );
-                })}
-              </tr>
-            ))}
-        </tbody>
-      </table>
+                          return (
+                            <div
+                              key={seat.id}
+                              style={{
+                                border: "1px solid rgb(191, 191, 191)",
+                                backgroundColor: bookedUser
+                                  ? "#f5b2aaff"
+                                  : "#afffa5ff",
+                                height: "24px",
+                              }}
+                              onClick={() =>
+                                handleBooking(
+                                  bookedUserId,
+                                  day.id,
+                                  time.id,
+                                  seat.id
+                                )
+                              }
+                            >
+                              {bookedUser || ""}
+                            </div>
+                          );
+                        })}
+                      </td>
+                    );
+                  })}
+                </tr>
+              ))}
+          </tbody>
+        </table>
+      </div>
       {alreadyBooked && (
         <div
           className="alreadyBooked-modal"
